9e44a9d38908145486b151e501928af9ae60b56f
João Pedro Matias <joao.matias@rebase.com.br>
2022-04-29 09:42:11 -0300

    Desafio finalizado

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,10 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(array), do: count(array, 0)
+
+  def count([], i), do: i
+
+  def count([x | xs], i) do
+    i = i + 1
+    count(xs, i)
   end
 end
